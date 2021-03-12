cea8c947835c9fdd197559c0b28cfbae3a269259
TELAGAMSETTI SAI TEJA <48620462+Sai999teja@users.noreply.github.com>
2021-03-12 17:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello_world():
 
 @app.route('/predict',methods=['POST','GET'])
 def predict():
-    int_features=[int(x) for x in request.form.values()]
+    int_features=[float(x) for x in request.form.values()]
     final=[np.array(int_features)]
     print(int_features)
     print(final)
@@ -26,10 +26,9 @@ def predict():
     output='{0:.{1}f}'.format(prediction[0][1], 2)
 
     if output>str(0.5):
-        return render_template('index.html',pred='There is a high of defaulting your loan')
+        return render_template('index.html',pred='There is a high chance of defaulting your loan, based on his previous record')
     else:
-        return render_template('index.html',pred='We can sanction him loan')
-
+        return render_template('index.html',pred='we can santion him a loan, based on his previous record')
 
 if __name__ == '__main__':
     app.run(debug=True)
